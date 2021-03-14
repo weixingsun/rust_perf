fec96e9fce79794aff5b7eb0bdfc6894a483f03e
Weixing Sun <weixing.sun@gmail.com>
2021-03-14 11:15:17 +0900

    move log

--- test1/target/release/deps/probe-024439361326110b.d
+++
@@ -1,9 +0,0 @@
-/home/weixingsun/deploy/scripts/rust/rust_perf/test1/target/release/deps/probe-024439361326110b.rmeta: /home/weixingsun/.cargo/git/checkouts/rust-libprobe-428800595a569dcb/ae1286b/src/lib.rs /home/weixingsun/.cargo/git/checkouts/rust-libprobe-428800595a569dcb/ae1286b/src/platform/mod.rs /home/weixingsun/.cargo/git/checkouts/rust-libprobe-428800595a569dcb/ae1286b/src/platform/systemtap.rs
-
-/home/weixingsun/deploy/scripts/rust/rust_perf/test1/target/release/deps/libprobe-024439361326110b.rlib: /home/weixingsun/.cargo/git/checkouts/rust-libprobe-428800595a569dcb/ae1286b/src/lib.rs /home/weixingsun/.cargo/git/checkouts/rust-libprobe-428800595a569dcb/ae1286b/src/platform/mod.rs /home/weixingsun/.cargo/git/checkouts/rust-libprobe-428800595a569dcb/ae1286b/src/platform/systemtap.rs
-
-/home/weixingsun/deploy/scripts/rust/rust_perf/test1/target/release/deps/probe-024439361326110b.d: /home/weixingsun/.cargo/git/checkouts/rust-libprobe-428800595a569dcb/ae1286b/src/lib.rs /home/weixingsun/.cargo/git/checkouts/rust-libprobe-428800595a569dcb/ae1286b/src/platform/mod.rs /home/weixingsun/.cargo/git/checkouts/rust-libprobe-428800595a569dcb/ae1286b/src/platform/systemtap.rs
-
-/home/weixingsun/.cargo/git/checkouts/rust-libprobe-428800595a569dcb/ae1286b/src/lib.rs:
-/home/weixingsun/.cargo/git/checkouts/rust-libprobe-428800595a569dcb/ae1286b/src/platform/mod.rs:
-/home/weixingsun/.cargo/git/checkouts/rust-libprobe-428800595a569dcb/ae1286b/src/platform/systemtap.rs:
--- test1/target/release/test1.d
+++
@@ -1,1 +0,0 @@
-/home/weixingsun/deploy/scripts/rust/rust_perf/test1/target/release/test1: /home/weixingsun/deploy/scripts/rust/rust_perf/test1/src/main.rs
