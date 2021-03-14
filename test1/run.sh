6283abd69bb1241eb4f824e2c0d387c7d2238ff6
Weixing Sun <weixing.sun@gmail.com>
2021-03-14 11:22:43 +0900

    minor fix

--- test1/run.sh
+++ test1/run.sh
@@ -3,6 +3,7 @@ cargo build --release
 tplist -l target/release/test1
 
 target/release/test1 &
+sleep 2
 sudo ../rust_perf -n test1 -b loop -i 1 -d 5 > loop.out &
 sudo ../rust_perf -n test1 -b add -i 1 -d 5 > add.out &
 sudo ../rust_perf -n test1 -b sub -i 1 -d 5 > sub.out &
