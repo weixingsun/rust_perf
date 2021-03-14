dcd864421afa715df48b648e3536a530fc74dbb9
Weixing Sun <weixing.sun@gmail.com>
2021-03-14 11:12:06 +0900

    fix: duplicated probe names when fn called many places

--- test1/run.sh
+++ test1/run.sh
@@ -1,5 +1,9 @@
+pkill test1
 cargo build --release
 tplist -l target/release/test1
 
 target/release/test1 &
-sudo ./rust_perf.py -n test1 -b loop -i 1 -d 5
+sudo ../rust_perf -n test1 -b loop -i 1 -d 5 > loop.out &
+sudo ../rust_perf -n test1 -b add -i 1 -d 5 > add.out &
+sudo ../rust_perf -n test1 -b sub -i 1 -d 5 > sub.out &
+sudo ../rust_perf -n test1 -b sleep -i 1 -d 5 > sleep.out &   # ['sleep__start', 'sleep__end', 'sleep__start', 'sleep__end']
--- test1/src/main.rs
+++ test1/src/main.rs
@@ -2,32 +2,35 @@
 
 use probe::probe;
 use std::{thread, time};
-
+fn sleep(a: u64){
+    probe!(test_2, sleep__start, a);
+    thread::sleep(time::Duration::from_millis(a));
+    probe!(test_2, sleep__end, a);
+}
 fn add(a: u64, b: u64) -> u64 {
-    probe!(probe, add__start, a, b);
-    thread::sleep(time::Duration::from_millis(10));
-    probe!(probe, add__end, a, b);
+    probe!(test_1, add__start, a, b);
+    sleep(10);
+    probe!(test_1, add__end, a, b);
     return a+b;
 }
 fn sub(a: u64, b: u64) -> u64 {
-    probe!(probe, sub__start, a);
-    thread::sleep(time::Duration::from_millis(20));
-    probe!(probe, sub__end, a);
+    probe!(test_1, sub__start, a);
+    sleep(20);
+    probe!(test_1, sub__end, a);
     return a-b;
 }
 
 fn infinite_loop(n: u64){
     let mut a: u64 = 10;
-    //let delay = time::Duration::from_millis(i);
     for i in 1..=n {
-        probe!(probe, loop__start);
+        probe!(test_0, loop__start);
         a = add(a, i);
-        //thread::sleep(delay);
+        //sleep(a);
         a = sub(a, i);
-        probe!(probe, loop__end);
+        probe!(test_0, loop__end);
     }
 }
 
 fn main() {
-    infinite_loop(1000);
+    infinite_loop(10000);
 }
